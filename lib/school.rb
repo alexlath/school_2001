166ad0790325f335b17fa240e5afbd227ca448c6
Alex Latham <alexlthm@gmail.com>
2020-01-29 13:10:23 -0700

    Add initialize method and attributes to School class

---
+++ lib/school.rb
@@ -0,0 +1,7 @@
+class School
+
+  def initialize(start_time_parameter, hours_in_school_day_parameter)
+    @start_time = start_time_parameter
+    @hours_in_school_day = hours_in_school_day_parameter
+  end
+end
