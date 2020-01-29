50388ba016b4a5482fc3dff6533af30557b74aa5
Alex Latham <alexlthm@gmail.com>
2020-01-29 13:15:04 -0700

    Add student_names method which passes test

--- lib/school.rb
+++ lib/school.rb
@@ -3,6 +3,7 @@ class School
   def initialize(start_time_parameter, hours_in_school_day_parameter)
     @start_time = start_time_parameter
     @hours_in_school_day = hours_in_school_day_parameter
+    @student_names  = []
   end
 
   def start_time
@@ -11,5 +12,9 @@ class School
 
   def hours_in_school_day
     @hours_in_school_day
+  end
+
+  def student_names
+    @student_names
   end
 end
